4a112cf2a335e0427ab7e332dc08359b67ddf6d3
Aidan Busby <aidan@debian>
2022-06-09 19:58:20 -0700

    add test

---
+++ test_rend.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<rpi-rgb-led-matrix/led-matrix-c.h>
+
+int main(int argc, char **argv){
+    struct RGBLedMatrixOptions options = {0};
+    struct RGBLedMatrix *ctx;
+    struct LedCanvas *vbuf;
+    int width, height;
+    int x, y;
+
+    options.rows = 32;
+    options.chain_length = 1;
+
+    ctx = led_matrix_create_from_options(&options, &argc, &argv);
+    vbuf = led_matrix_create_offscreen_canvas(ctx);
+    led_canvas_get_size(vbuf, &width, &height);
+
+    for (y = 0; y < height; y++){
+        for (x = 0; x < height; x++){
+            led_canvas_set_pixel(vbuf, x, y, 255, 255, 255);
+        }
+    }
+    vbuf = led_matrix_swap_on_vsync(ctx, vbuf);
+
+}
