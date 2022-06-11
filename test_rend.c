a8019714af88c81d75a0412d27f55e3bec1316cd
Aidan Busby <aidan@debian>
2022-06-10 17:30:23 -0700

    test

--- test_rend.c
+++ test_rend.c
@@ -15,13 +15,11 @@ int main(int argc, char **argv){
     vbuf = led_matrix_create_offscreen_canvas(ctx);
     led_canvas_get_size(vbuf, &width, &height);
     while (1){
-        for (y = 0; y < height; y++){
-            for (x = 0; x < width; x++){
+        for (y = 0; y < height; ++y){
+            for (x = 0; x < width; ++x{
                 led_canvas_set_pixel(vbuf, x, y, 255, 255, 255);
             }
         }
-        x = 0;
-        y = 0;
         vbuf = led_matrix_swap_on_vsync(ctx, vbuf);
     }
     led_matrix_delete(ctx);
