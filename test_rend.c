561bcfabfcda917da3b5331f8441050f6815e60e
Aidan Busby <aidan@debian>
2022-06-12 18:48:57 -0700

    rainbow

--- test_rend.c
+++ test_rend.c
@@ -17,9 +17,34 @@ int main(int argc, char **argv){
     vbuf = led_matrix_create_offscreen_canvas(ctx);
     led_canvas_get_size(vbuf, &width, &height);
     while (1){
-        for (y = 0; y < height; ++y){
-            for (x = 0; x < width; ++x){
-                led_canvas_set_pixel(vbuf, x, y, 64-x, 32-x, x);
+        for (y = 0; y < height; y++){
+            for (x = 0; x < (1/6)*x; x++){
+                led_canvas_set_pixel(vbuf, x, y, 64, 6*x, 0);
+            }
+        }
+        for (y = 0; y < height; y++){
+            for (x = width/6; x < (2/6)*width; x++){
+                led_canvas_set_pixel(vbuf, x, y, 64-(6*x), 64, 0);
+            }
+        }
+        for (y = 0; y < height; y++){
+            for (x = (2/6)*width; x < (3/6)*width; x++){
+                led_canvas_set_pixel(vbuf, x, y, 0, 64, 6*x);
+            }
+        }
+        for (y = 0; y < height; y++){
+            for (x = (3/6)*width; x < (4/6)*width; x++){
+                led_canvas_set_pixel(vbuf, x, y, 0, 64-(6*x), 64);
+            }
+        }
+        for (y = 0; y < height; y++){
+            for (x = (4/6)*width; x < (5/6)*width; x++){
+                led_canvas_set_pixel(vbuf, x, y, 6*x, 0, 64);
+            }
+        }
+        for (y = 0; y < height; y++){
+            for (x = (5/6)*width; x < width; x++){
+                led_canvas_set_pixel(vbuf, x, y, 64, 0, 64-(6*x));
             }
         }
         vbuf = led_matrix_swap_on_vsync(ctx, vbuf);
