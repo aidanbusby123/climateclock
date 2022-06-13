93a7a5a62eba02e277abd334050d6224392162fc
Aidan Busby <aidan@debian>
2022-06-12 19:14:20 -0700

    rainbow

--- test_rend.c
+++ test_rend.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv){
     struct RGBLedMatrix *ctx;
     struct LedCanvas *vbuf;
     int width, height;
-    int x, y;
+    int x, y, i;
 
     options.rows = 32;
     options.chain_length = 1;
@@ -18,33 +18,45 @@ int main(int argc, char **argv){
     led_canvas_get_size(vbuf, &width, &height);
     while (1){
         for (y = 0; y < height; y++){
+            i = 0;
             for (x = 0; x < width/6; x++){
-                led_canvas_set_pixel(vbuf, x, y, 64, 6*x, 0);
+                led_canvas_set_pixel(vbuf, x, y, 64, 6*i, 0);
+                i++;
             }
         }
         for (y = 0; y < height; y++){
+            i = 0;
             for (x = width/6; x < (width*2)/6; x++){
-                led_canvas_set_pixel(vbuf, x, y, 64-(6*x), 64, 0);
+                led_canvas_set_pixel(vbuf, x, y, 64-(6*i), 64, 0);
+                i++;
             }
         }
         for (y = 0; y < height; y++){
+            i = 0;
             for (x = (width*2)/6; x < (width*3)/6; x++){
-                led_canvas_set_pixel(vbuf, x, y, 0, 64, 6*x);
+                led_canvas_set_pixel(vbuf, x, y, 0, 64, 6*i);
+                i++;
             }
         }
         for (y = 0; y < height; y++){
+            i = 0;
             for (x = (3*width)/6; x < (width*4)/6; x++){
-                led_canvas_set_pixel(vbuf, x, y, 0, 64-(6*x), 64);
+                led_canvas_set_pixel(vbuf, x, y, 0, 64-(6*i), 64);
+                i++;
             }
         }
         for (y = 0; y < height; y++){
+            i = 0;
             for (x = (width*4)/6; x < (5*width)/6; x++){
-                led_canvas_set_pixel(vbuf, x, y, 6*x, 0, 64);
+                led_canvas_set_pixel(vbuf, x, y, 6*i, 0, 64);
+                i++;
             }
         }
         for (y = 0; y < height; y++){
+            i = 0;
             for (x = (5*width)/6; x < width; x++){
-                led_canvas_set_pixel(vbuf, x, y, 64, 0, 64-(6*x));
+                led_canvas_set_pixel(vbuf, x, y, 64, 0, 64-(6*i));
+                i++;
             }
         }
         vbuf = led_matrix_swap_on_vsync(ctx, vbuf);
